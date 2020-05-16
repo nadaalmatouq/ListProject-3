5febffc1ea084cd1d0de57dae9552b26d1bf6947
nada <64605593+nadaalmatouq@users.noreply.github.com>
2020-05-16 03:43:41 +0300

    Create New List View is almost Done, Organized the assets folders

---
+++ ListProject/Views/Env.swift
@@ -0,0 +1,16 @@
+//
+//  Env.swift
+//  ListProject
+//
+//  Created by Nada Alm on 5/15/20.
+//  Copyright © 2020 Alyaa. All rights reserved.
+//
+
+import Foundation
+class Env: ObservableObject{
+
+    @Published var ListName: [String] = ["Travel List","Shopping List","Celebration List"]
+    
+
+
+}//End class Env
---
+++ ListProject/Views/TravelListDetails.swift
@@ -0,0 +1,31 @@
+//
+//  TravelListDetails.swift
+//  ListProject
+//
+//  Created by Nada Alm on 5/16/20.
+//  Copyright © 2020 Alyaa. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct TravelListDetails: View {
+    
+    
+    
+    var body: some View {
+        VStack {
+      Text("Hello")
+        }
+    }
+}
+
+
+
+
+
+struct TravelListDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        TravelListDetails()
+    }
+}
